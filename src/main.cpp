8ffbe5a81209da7ed62b64c74932dc3b236afe9f
Jan <107766070+Jan-Stangelj@users.noreply.github.com>
2024-12-29 18:13:30 +0100

    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -72,27 +72,31 @@ int main(){
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
     
+    // Mesh
     Renderer::VAO VAO;
-
     Renderer::VBO VBO(vertices, sizeof(vertices));
-
     VAO.addAttribute(Renderer::vertexAttribute(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0));
     VAO.addAttribute(Renderer::vertexAttribute(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))));
 
+    // Textures
     Renderer::Texture texture("../assets/textures/red_brick.jpg", 0, shader, "texture1", 3);
     Renderer::Texture texture2("../assets/textures/awesomeface.png", 1, shader, "texture2", 4);
     texture.Bind(0, shader, "texture1");
     texture2.Bind(1, shader, "texture2");
 
+    // Camera
     Renderer::Camera cam(60.0f, 1920.0f/1080.0f, 0.1f, 100.0f, shader);
     cam.setPosition(glm::vec3(0.0f, 0.0f, -3.0f));
 
+    // Main loop
     while (!Window.shouldWindowClose()){
         Window.clear(glm::vec4(0.2f, 0.3f, 0.3f, 1.0f));
 
+        // Camera movement calls
         cam.cameraMovement(Window, Window.deltaTime(), 1.0f);
         cam.cameraRotation(0.05f, mouseX, mouseY);
 
+        // Object translation
         glm::mat4 trans = glm::mat4(1.0f);
         trans = glm::rotate(trans, glm::radians(45.0f), glm::vec3(1.0, 1.0, 1.0));
         shader.setMat4("model", trans);
